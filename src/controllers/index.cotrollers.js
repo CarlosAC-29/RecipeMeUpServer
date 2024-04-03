f4d226dc327babe340b84c7795c1c31ca8a9a089
Carlos Caceres <carlosmicro33@gmail.com>
2024-04-03 18:29:44 -0500

    refactor : env variables

--- src/controllers/index.cotrollers.js
+++ src/controllers/index.cotrollers.js
@@ -71,73 +71,14 @@ const geminiApi = async (req, res) => {
 
 };
 
-const client = new SessionsClient({ apiEndpoint: 'us-central1-dialogflow.googleapis.com', ...dialogflowConfig });
-
-
-// Endpoint para manejar las consultas
-// const dialogFlowApi = async (req, res) => {
-//     const { userQuery } = req.body;
-//     if (!userQuery) {
-//         return res.status(400).json({ error: 'Falta el campo "userQuery" en el cuerpo de la solicitud' });
-//     }
-//     try {
-//         const response = await detectIntentText(userQuery);
-//         res.json(response);
-//     } catch (error) {
-//         console.error('Error al procesar la consulta:', error);
-//         res.status(500).json({ error: 'Hubo un error al procesar la consulta' });
-//     }
-// };
-
-// // Función para detectar la intención del texto
-// async function detectIntentText(query) {
-//     const projectId = "recipemeup";
-//     const location = 'us-central1';
-//     const agentId = 'b4a099bb-e6b7-489a-b264-38e1284a59f3';
-//     const languageCode = 'es';
-
-//     const sessionId = Math.random().toString(36).substring(7);
-//     const sessionPath = client.projectLocationAgentSessionPath(
-//         projectId,
-//         location,
-//         agentId,
-//         sessionId
-//     );
-//     const request = {
-//         session: sessionPath,
-//         queryInput: {
-//             text: {
-//                 text: query,
-//             },
-//             languageCode,
-//         },
-//     };
-//     const [response] = await client.detectIntent(request);
-//     const result = {
-//         messages: [],
-//         matchedIntent: '',
-//         currentPage: '',
-//     };
-//     for (const message of response.queryResult.responseMessages) {
-//         if (message.text) {
-//             result.messages.push(message.text.text);
-//         }
-//     }
-//     if (response.queryResult.match.intent) {
-//         result.matchedIntent = response.queryResult.match.intent.displayName;
-//     }
-//     result.currentPage = response.queryResult.currentPage.displayName;
-//     return result;
-// }
-
 // Función para enviar audio a Dialogflow CX
 const AudioToDialogflowCX = async (audioFilePath) => {
     const sessionId = uuid.v4();
-    const sessionClient = new SessionsClient({ apiEndpoint: "us-central1-dialogflow.googleapis.com", ...dialogflowConfig });
+    const sessionClient = new SessionsClient({ apiEndpoint: dialogflowConfig.apiEndpoint, ...dialogflowConfig });
     const sessionPath = sessionClient.projectLocationAgentSessionPath(
-        "recipemeup",
+        dialogflowConfig.project_id,
         "us-central1",
-        "b4a099bb-e6b7-489a-b264-38e1284a59f3",
+        dialogflowConfig.agent_id,
         sessionId
     );
 
